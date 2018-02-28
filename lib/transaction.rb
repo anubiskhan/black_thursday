ee829ceca6e0333e09bb5661e72be2118b3d8841
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-02-27 18:48:39 -0700

    cleans up transaction class

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 require 'bigdecimal'
 require 'time'
+
+# builds transaction class
 class Transaction
   attr_reader :id,
               :invoice_id,
@@ -10,17 +14,17 @@ class Transaction
               :updated_at
 
   def initialize(data, parent)
-    @id = data[:id].to_i
-    @invoice_id = data[:invoice_id].to_i
-    @credit_card_number = data[:credit_card_number].to_i
+    @id                          = data[:id].to_i
+    @invoice_id                  = data[:invoice_id].to_i
+    @credit_card_number          = data[:credit_card_number].to_i
     @credit_card_expiration_date = data[:credit_card_expiration_date]
-    @result = data[:result]
-    @created_at  = Time.parse(data[:created_at])
-    @updated_at  = Time.parse(data[:updated_at])
-    @parent = parent
+    @result                      = data[:result]
+    @created_at                  = Time.parse(data[:created_at])
+    @updated_at                  = Time.parse(data[:updated_at])
+    @parent                      = parent
   end
 
   def invoice
-    @parent.transaction_repo_finds_invoice_via_engine(self.invoice_id)
+    @parent.transaction_repo_finds_invoice_via_engine(invoice_id)
   end
 end
