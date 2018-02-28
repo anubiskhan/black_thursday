eb2cfad3179c11da09c2c9bc9bd0667c5873410a
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-02-27 18:39:03 -0700

    fixes multiple indentation issues

--- lib/customer.rb
+++ lib/customer.rb
@@ -10,14 +10,13 @@ class Customer
               :last_name,
               :created_at,
               :updated_at
-
   def initialize(data, parent)
-    @id = data[:id].to_i
+    @id         = data[:id].to_i
     @first_name = data[:first_name]
-    @last_name = data[:last_name]
-    @created_at  = Time.parse(data[:created_at])
-    @updated_at  = Time.parse(data[:updated_at])
-    @parent = parent
+    @last_name  = data[:last_name]
+    @created_at = Time.parse(data[:created_at])
+    @updated_at = Time.parse(data[:updated_at])
+    @parent     = parent
   end
 
   def merchants
--- lib/invoice.rb
+++ lib/invoice.rb
@@ -5,15 +5,20 @@ require 'time'
 
 # builds invoice class
 class Invoice
-  attr_reader :id, :customer_id, :merchant_id, :status, :created_at, :updated_at
+  attr_reader :id,
+              :customer_id,
+              :merchant_id,
+              :status,
+              :created_at,
+              :updated_at
   def initialize(hash, parent)
-    @id = hash[:id].to_i
+    @id          = hash[:id].to_i
     @customer_id = hash[:customer_id].to_i
     @merchant_id = hash[:merchant_id].to_i
-    @status = hash[:status].to_sym
-    @created_at = Time.parse(hash[:created_at])
-    @updated_at = Time.parse(hash[:updated_at])
-    @parent = parent
+    @status      = hash[:status].to_sym
+    @created_at  = Time.parse(hash[:created_at])
+    @updated_at  = Time.parse(hash[:updated_at])
+    @parent      = parent
   end
 
   def merchant
