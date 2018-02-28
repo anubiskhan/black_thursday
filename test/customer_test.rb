f58eeea9c075d4a8380f0ea6a421c6a4989aa989
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-02-27 18:59:41 -0700

    cleans up customer test

--- test/customer_test.rb
+++ test/customer_test.rb
@@ -1,21 +1,25 @@
+# frozen_string_literal: true
+
 require 'bigdecimal'
-require_relative 'test_helper'
-require_relative '../lib/customer'
-require_relative '../lib/sales_engine'
-require_relative './master_hash'
-require 'pry'
+require_relative 'test_helper.rb'
+require_relative '../lib/customer.rb'
+require_relative '../lib/sales_engine.rb'
+require_relative './master_hash.rb'
 
 class CustomerTest < Minitest::Test
   def setup
     test_engine = TestEngine.new.god_hash
     @sales_engine = SalesEngine.new(test_engine)
-    @customer = Customer.new({
-      :id        => 7,
-      :first_name => "Mary",
-      :last_name  => "Jane",
-      :created_at  => '2018-02-21 19:23:23 UTC',
-      :updated_at  => '2018-02-21 19:23:23 UTC',
-      }, @sales_engine.customers)
+    @customer = Customer.new(
+      {
+        id: 7,
+        first_name: 'Mary',
+        last_name: 'Jane',
+        created_at: '2018-02-21 19:23:23 UTC',
+        updated_at: '2018-02-21 19:23:23 UTC'
+      },
+      @sales_engine.customers
+    )
   end
 
   def test_it_exists
@@ -26,24 +30,27 @@ class CustomerTest < Minitest::Test
 
   def test_it_has_attributes
     assert_equal 7, @customer.id
-    assert_equal "Mary", @customer.first_name
-    assert_equal "Jane", @customer.last_name
+    assert_equal 'Mary', @customer.first_name
+    assert_equal 'Jane', @customer.last_name
     assert_instance_of Time, @customer.created_at
     assert_instance_of Time, @customer.updated_at
   end
 
   def test_it_can_have_different_attributes
-    customer2 = Customer.new({
-      :id        => 12,
-      :first_name => "Bob",
-      :last_name  => "Hope",
-      :created_at  => '2018-12-01 19:23:23 UTC',
-      :updated_at  => '2018-12-01 19:23:23 UTC',
-      }, @sales_engine.customers)
+    customer2 = Customer.new(
+      {
+        id: 12,
+        first_name: 'Bob',
+        last_name: 'Hope',
+        created_at: '2018-12-01 19:23:23 UTC',
+        updated_at: '2018-12-01 19:23:23 UTC'
+      },
+      @sales_engine.customers
+    )
 
     assert_equal 12, customer2.id
-    assert_equal "Bob", customer2.first_name
-    assert_equal "Hope", customer2.last_name
+    assert_equal 'Bob', customer2.first_name
+    assert_equal 'Hope', customer2.last_name
     assert_instance_of Time, customer2.created_at
     assert_instance_of Time, customer2.updated_at
   end
